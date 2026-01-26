b388ecadd7675cfaf3d6e1316fb1a5bcadec96e2
Spatula-code <info@spatula.es>
2026-01-26 14:15:01 +0100

    Create maudies.config.js

---
+++ config/maudies.config.js
@@ -0,0 +1,11 @@
+export default {
+  restaurant: {
+    id: "maudies",
+    name: "Maudie’s",
+    description: "Pregunta lo que quieras sobre nuestra carta"
+  },
+
+  typing: {
+    speed: 15
+  }
+};
